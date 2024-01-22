71b9ef09bdfc958a053d918a97be47a362595245
Shreyans Raj <shreyans00>
2024-01-22 14:51:23 +0530

    first

---
+++ server/app.js
@@ -0,0 +1,9 @@
+require("dotenv");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello"));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
